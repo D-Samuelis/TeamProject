029176a4af8b8eb85d13de19df7314e4e773ee6d
nicolas-droppa <droppa.nicolas@gmail.com>
2026-03-28 23:40:23 +0100

    fix: Fix layout issues with business index headert

--- laravel/resources/views/pages/private/business/index.blade.php
+++ laravel/resources/views/pages/private/business/index.blade.php
@@ -25,40 +25,48 @@
     </aside>
 
     <main class="business__main">
-        <header class="business__header-wrapper">
+        <header class="business__header-wrapper business__header-wrapper--simple">
+            <div class="business__header-corner"></div>
+
             <div class="business__header-info">
-                <h2 class="business-header__title">My Businesses</h2>
-                
-                <div class="business-info">
-                    <div class="stat-item stat-item--all">
-                        <i class="fa-solid fa-layer-group"></i>
-                        <div id="countAll">0</div> Total businesses
-                    </div>
-                    <div class="stat-item stat-item--published">
-                        <i class="fa-solid fa-circle-check"></i>
-                        <div id="countPublished">0</div> Published
-                    </div>
-                    <div class="stat-item stat-item--hidden">
-                        <i class="fa-solid fa-eye-slash"></i>
-                        <div id="countHidden">0</div> Hidden
-                    </div>
-                    <div class="stat-item stat-item--deleted">
-                        <i class="fa-solid fa-trash"></i>
-                        <div id="countDeleted">0</div> Archived
+                <div class="business__header-info">
+                    <h2 class="business-header__title">My Businesses</h2>
+                    
+                    <div class="business-info">
+                        <div class="stat-item stat-item--all">
+                            <i class="fa-solid fa-layer-group"></i>
+                            <div id="countAll">0</div> Total businesses
+                        </div>
+                        <div class="stat-item stat-item--published">
+                            <i class="fa-solid fa-circle-check"></i>
+                            <div id="countPublished">0</div> Published
+                        </div>
+                        <div class="stat-item stat-item--hidden">
+                            <i class="fa-solid fa-eye-slash"></i>
+                            <div id="countHidden">0</div> Hidden
+                        </div>
+                        <div class="stat-item stat-item--deleted">
+                            <i class="fa-solid fa-trash"></i>
+                            <div id="countDeleted">0</div> Archived
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="business__search-wrapper">
-                <div class="search-container">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <input type="text" id="businessSearchInput" placeholder="Search businesses...">
+            <div class="business__header-right">
+                <div class="business__header-right-section_1"> </div>
+                <div class="business__header-right-section_2">
+                    <div class="list-view__search-wrapper">
+                        <div class="search-container">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <input type="text" id="businessSearchInput" placeholder="Search businesses...">
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
 
         <div class="business__body-wrapper">
-            {{-- JS GENERATED TABLE --}}
             <div id="businessTableContainer" class="list-view__body-wrapper"></div>
         </div>
     </main>
