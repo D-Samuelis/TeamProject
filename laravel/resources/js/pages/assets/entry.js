095c569976df02934adbdfecc26850c9606984ad
nicolas-droppa <droppa.nicolas@gmail.com>
2026-04-02 15:25:35 +0200

    chore: Prepare for merge

--- laravel/resources/js/pages/assets/entry.js
+++ laravel/resources/js/pages/assets/entry.js
@@ -4,34 +4,9 @@ import { initRuleDragDrop } from './dragDrop.js';
 document.addEventListener('DOMContentLoaded', () => {
     initCollapsibleList('assetInfo');
     initCollapsibleList('branchesList');
-    initCollapsibleList('actionsList');
     const container = document.querySelector('.rule-panel');
 
     initRuleDragDrop({
         reorderUrl: container ? container.dataset.reorderUrl : null
     });
-    /* 
-    initBusinessListView(window.BE_DATA.businesses);
-    initBusinessStatusFilters();
-
-    initCreateBusinessModal();
-    initEditBusinessMetaDataModal();
-
-    initCreateBranchModal();
-    initEditBranchModal();
-
-    initCollapsibleList('businessInfo');
-    initCollapsibleList('branchesList');
-    initCollapsibleList('manageEmployeesList');
-
-    initBusinessViewSwitcher();
-
-    initAssignEmployeeModal();
-
-    initListSearch('#appointmentSearchInput', '.filterable-member', '.js-search-data');
-    initListSearch('#appointmentSearchInput', '.filterable-service', 'strong');
-
-    initArchiveBranchModal();
-    initRemoveUserModal();
-    initArchiveBusinessModal(); */
 });
