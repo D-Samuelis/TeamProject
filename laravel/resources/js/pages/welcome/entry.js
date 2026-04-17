706966b4ba278797cae5467bfe68d18f99b982b5
nicolas-droppa <droppa.nicolas@gmail.com>
2026-04-17 14:13:22 +0200

    Remove input from welcome page and add dashboard link

--- laravel/resources/js/pages/welcome/entry.js
+++ laravel/resources/js/pages/welcome/entry.js
@@ -1,9 +1,3 @@
-import { setRandomPlaceholder } from '../../components/helpers/textGenerator.js';
-import { APPOINTMENT_SAMPLES } from '../../config/constants.js';
-
 document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('aiBookingInput');
-    if (input) {
-        setRandomPlaceholder(input, APPOINTMENT_SAMPLES);
-    }
+    //empty for now
 });
