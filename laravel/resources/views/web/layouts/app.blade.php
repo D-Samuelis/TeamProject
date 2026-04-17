6a6430fdcceceefbc5f3a426001719428c3bd731
nicolas-droppa <droppa.nicolas@gmail.com>
2026-04-17 21:37:09 +0200

    feat: Add favicon

--- laravel/resources/views/web/layouts/app.blade.php
+++ laravel/resources/views/web/layouts/app.blade.php
@@ -3,6 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
     <title>@yield('title', 'Home')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
